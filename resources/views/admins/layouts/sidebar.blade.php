e1d73e784848df18867a7c68f21da4b4b77614fe
thanhlam200496 <thanhlam200496@gmail.com>
2024-09-09 15:56:13 +0700

    update sidebar admin

--- resources/views/admins/layouts/sidebar.blade.php
+++ resources/views/admins/layouts/sidebar.blade.php
@@ -68,6 +68,15 @@
                     </ul>
                 </li> --}}
                 <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                            Category
+                            {{-- <span class="right badge badge-danger">New</span> --}}
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a href="{{route('Home')}}" class="nav-link">
                         <i class="nav-icon fas fa-th"></i>
                         <p>
