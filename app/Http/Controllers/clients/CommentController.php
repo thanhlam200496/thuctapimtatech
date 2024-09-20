e9ee37c2f21ef50786330bc5082ff9fb0c289cb1
npminhh <mink2kar4@gmail.com>
2024-09-20 18:09:28 +0700

    formcmt

--- app/Http/Controllers/clients/CommentController.php
+++ app/Http/Controllers/clients/CommentController.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Http\Controllers\clients;
-
+use App\Models\Comment;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    function addcomment(Request $req) {
+        $req->validate(['comments_content'=>'required'],);
+        Comment::create($req->all('article_id','user_id','comments_content'));
+        return redirect()->back()->with(['message'=>'Bình luận thành công']);
+    }
+
 }
