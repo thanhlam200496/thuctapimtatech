ce3c3ca72df8b95a43d1e052c0e54a8c8d0fec5b
LocNguyen0 <locmcpe123@gmail.com>
2024-10-05 17:54:51 +0700

    Sửa giao diện admin

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -2,18 +2,16 @@
 
 @section('title', 'Chỉnh Sửa')
 
-@section('title-page', 'Chỉnh Sửa Loại Tin')
-
 @section('main-content')
     <!-- Main content -->
     <section class="container mt-4">
 
         <!-- Default box -->
-        <div class="col-md-6 mx-auto">
+        
             <!-- general form elements -->
-            <div class="card">
-                <div class="card-header bg-primary text-white">
-                    <h3 class="card-title">Chỉnh Sửa Loại Tin: {{ $category->name }}</h3>
+            <div class="card card-primary">
+                <div class="card-header bg-primary text-white" style="padding: 12px 20px 12px 20px; border-radius: 7px 7px 0 0">
+                    <h3 class="card-title text-white">Chỉnh Sửa Loại Tin: {{ $category->name }}</h3>
                 </div>
                 <!-- /.card-header -->
                 <!-- form start -->
@@ -22,7 +20,7 @@
                     @csrf
                     <input type="hidden" name="id" value="{{ $category->id }}">
                     <div class="card-body">
-                        <div class="mb-3">
+                        <div class="form-group mb-3">
                             <label for="name" class="form-label">Tên Loại Tin</label>
                             <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name='name'
                                 value="{{ old('name') ? old('name') : $category->name }}">
@@ -30,31 +28,20 @@
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="form-group mb-3 @error('name') has-error @enderror">
+
+                        <div class="form-group mb-3">
                             <label for="parent_id" class="form-label">Danh mục cha</label>
-                            <select name="parent_id" class="form-control" id="">
+                            <select name="parent_id" class="form-control">
                                 <option value="">--Trống--</option>
                                 @foreach ($categories as $item)
-                                @if ($item->id==$category->parent_id)
-                                    <option value="{{$item->id}}" selected>{{$item->name}}</option>
-                                @else
-                                <option value="{{$item->id}}" >{{$item->name}}</option>
-                                @endif
-                                    
+                                    <option value="{{ $item->id }}" {{ $item->id == $category->parent_id ? 'selected' : '' }}>
+                                        {{ $item->name }}
+                                    </option>
                                 @endforeach
                             </select>
-                            
                         </div>
-                        {{-- <div class="mb-3">
-                            <label for="parent_id" class="form-label">Chọn LT-Cha</label>
-                            <select name="parent_id" id="parent_id" class="form-select">
-                                <option value="">Chọn LT-Cha</option>
-                                @foreach ($categories as $item)
-                                    <option value="{{ $item->id }}" {{ $category->parent_id == $item->id ? 'selected' : '' }}>{{ $item->name }}</option>
-                                @endforeach
-                            </select>
-                        </div> --}}
-                        <div class="mb-3">
+
+                        <div class="form-group mb-3">
                             <label class="form-label">Chọn trạng thái</label>
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="status" id="status1" value="1"
@@ -74,14 +61,16 @@
                     </div>
                     <!-- /.card-body -->
 
-                    <div class="card-footer">
+                    <div class="card-footer text-end">
                         <button type="submit" class="btn btn-primary">Cập nhật</button>
                     </div>
                 </form>
             </div>
             <!-- /.card -->
-        </div>
+
+        
         <!-- /.col -->
+
     </section>
     <!-- /.content -->
 @endsection
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.master')
 
-@section('title', 'Danh Sách')
+@section('title', 'Danh Sách Loại Tin')
 
 @section('title-page', 'Danh Sách Loại Tin')
 
@@ -20,8 +20,7 @@
                 <div class="box-header d-flex justify-content-between align-items-center">
                     <div>
                         <a href="{{ route('category.create') }}" class="btn btn-success">+ Thêm mới Loại Tin</a>
-                        <a href="{{ route('category.trash') }}" class="btn btn-primary"><i class="fa fa-trash"></i> Thùng
-                            Rác</a>
+                        {{-- <a href="{{ route('category.trash') }}" class="btn btn-secondary"><i class="fa fa-trash"></i> Thùng Rác</a> --}}
                     </div>
                     <div class="box-tools">
                         <form action="{{ route('category.index') }}" method="GET">
@@ -36,75 +35,34 @@
                     </div>
                 </div>
                 <!-- /.box-header -->
-                <div class="box-body table-responsive no-padding">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>Stt</th>
-                                <th>Tên Loại Tin</th>
-                                <th>LT-Cha</th>
-                                <th>Trạng thái</th>
-                                <th>Ngày tạo</th>
-                                <th>Tùy chọn</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($categories as $item)
-                                @if ($item->parent_id == null)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $item->name }}</td>
-                                        <td>{{ $item->parent_id }}</td>
-                                        <td>{!! $item->status
-                                            ? '<span class="label label-success">Hiển Thị</span>'
-                                            : '<span class="label label-danger">Ẩn</span>' !!}</td>
-                                        <td>{{ $item->created_at->format('d/m/Y') }}</td>
-                                        <td>
-                                            <a href="{{ route('category.edit', $item) }}" class="btn btn-success"><i
-                                                    class="fa fa-pencil"></i> Chỉnh Sửa</a>
-                                            <form action="{{ route('category.destroy', $item) }}" method="POST"
-                                                style="display:inline;">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-danger"
-                                                    onclick="return confirm('Bạn Chắc Muốn Xóa?')"><i
-                                                        class="fa fa-trash"></i> Xóa</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                    @foreach ($categories as $item2)
-                                        @if ($item->id==$item2->parent_id)
-                                        <tr>
-                                            <td>+</td>
-                                            <td>{{ $item2->name }}</td>
-                                            <td>@if ($item->id==$item2->parent_id)
-                                                {{$item->name}}
-                                                @endif
-                                            </td>
-                                            <td>{!! $item2->status
-                                                ? '<span class="label label-success">Hiển Thị</span>'
-                                                : '<span class="label label-danger">Ẩn</span>' !!}</td>
-                                            <td>{{ $item2->created_at->format('d/m/Y') }}</td>
-                                            <td>
-                                                <a href="{{ route('category.edit', $item2) }}" class="btn btn-success"><i
-                                                        class="fa fa-pencil"></i> Chỉnh Sửa</a>
-                                                <form action="{{ route('category.destroy', $item2) }}" method="POST"
-                                                    style="display:inline;">
-                                                    @method('DELETE')
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-danger"
-                                                        onclick="return confirm('Bạn Chắc Muốn Xóa?')"><i
-                                                            class="fa fa-trash"></i> Xóa</button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                        @endif
-                                    @endforeach
-                                    @endif
-                                    
-                                {{-- @else
+            </div>
+            <!-- /.box -->
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+              <div class="">
+                <div class="card-header bg-primary text-white" style="padding: 12px 20px 12px 20px; border-radius: 7px 7px 0 0">
+                    <p class="card-title text-white" style="font-weight: 600; font-size: 1.2rem">Danh sách loại tin</p>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body table-responsive p-0" style="height: 430px;">
+                  <table class="table table-hover table-head-fixed text-nowrap">
+                    <thead>
+                        <tr>
+                            <th>Stt</th>
+                            <th>Tên Loại Tin</th>
+                            <th>LT-Cha</th>
+                            <th>Trạng thái</th>
+                            <th>Ngày tạo</th>
+                            <th>Tùy chọn</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($categories as $item)
+                            @if ($item->parent_id == null)
                                 <tr>
-                                    <td>+</td>
+                                    <td>{{ $loop->iteration }}</td>
                                     <td>{{ $item->name }}</td>
                                     <td>{{ $item->parent_id }}</td>
                                     <td>{!! $item->status
@@ -112,31 +70,49 @@
                                         : '<span class="label label-danger">Ẩn</span>' !!}</td>
                                     <td>{{ $item->created_at->format('d/m/Y') }}</td>
                                     <td>
-                                        <a href="{{ route('category.edit', $item) }}" class="btn btn-success"><i
-                                                class="fa fa-pencil"></i> Chỉnh Sửa</a>
-                                        <form action="{{ route('category.destroy', $item) }}" method="POST"
-                                            style="display:inline;">
+                                        <a href="{{ route('category.edit', $item) }}" class="btn btn-success"><i class="fa fa-pencil"></i> Chỉnh Sửa</a>
+                                        <form action="{{ route('category.destroy', $item) }}" method="POST" style="display:inline;">
                                             @method('DELETE')
                                             @csrf
-                                            <button type="submit" class="btn btn-danger"
-                                                onclick="return confirm('Bạn Chắc Muốn Xóa?')"><i
-                                                    class="fa fa-trash"></i> Xóa</button>
+                                            <button type="submit" class="btn btn-danger" onclick="return confirm('Bạn Chắc Muốn Xóa?')"><i class="fa fa-trash"></i> Xóa</button>
                                         </form>
                                     </td>
-                                </tr> --}}
-                                
-                            @empty
-                                <tr>
-                                    <td colspan="5" class="text-center">Chưa Có Dữ Liệu</td>
                                 </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
+
+                                @foreach ($categories as $item2)
+                                    @if ($item->id == $item2->parent_id)
+                                        <tr>
+                                            <td>+</td>
+                                            <td>{{ $item2->name }}</td>
+                                            <td>{{ $item->name }}</td>
+                                            <td>{!! $item2->status
+                                                ? '<span class="label label-success">Hiển Thị</span>'
+                                                : '<span class="label label-danger">Ẩn</span>' !!}</td>
+                                            <td>{{ $item2->created_at->format('d/m/Y') }}</td>
+                                            <td>
+                                                <a href="{{ route('category.edit', $item2) }}" class="btn btn-success"><i class="fa fa-pencil"></i> Chỉnh Sửa</a>
+                                                <form action="{{ route('category.destroy', $item2) }}" method="POST" style="display:inline;">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Bạn Chắc Muốn Xóa?')"><i class="fa fa-trash"></i> Xóa</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            @endif
+                        @empty
+                            <tr>
+                                <td colspan="6" class="text-center">Chưa Có Dữ Liệu</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                  </table>
                 </div>
-                <!-- /.box-body -->
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
             </div>
-            <!-- /.box -->
-        </div>
-        <!-- /.box -->
+          </div>
     </section>
 @endsection
